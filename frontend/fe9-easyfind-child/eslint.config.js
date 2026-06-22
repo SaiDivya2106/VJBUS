6388403e4eeb7809c699c89792ab902fd0d03373
Bandarupallipavani <pavanibandarupally2@gmail.com>
2026-06-22 15:56:04 +0530

    issue resolved

--- frontend/fe9-easyfind-child/eslint.config.js
+++ frontend/fe9-easyfind-child/eslint.config.js
@@ -22,6 +22,7 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'react': react,
     },
     rules: {
       ...js.configs.recommended.rules,
